a59ca2bdbd57388b719311dd72eba322f56756b4
mariomnzc <mariomnzc@gmail.com>
2021-05-05 19:52:50 -0400

    Descargar fichas técnicas

--- contratos/post_perfiles.php
+++ contratos/post_perfiles.php
@@ -1,13 +1,51 @@
-<?php require('../coneccion/datos_coneccion.php');
-            
-            if(isset($_POST['crearperfil'])){
-                        
-                $nombre_perfil = $_POST['perfil'];
-                print($nombre_perfil);
-                $consulta="INSERT INTO TIENE_06  FROM PERFIL p WHERE p.NOMBRE_PERFIL='$nombre_perfil' ";
-            }
-            else{
-                echo "<h1>aqui no pasa nada<h1>";
-            }
+<?php
 
-        ?>
+    require('fpdf.php');
+
+    class PDF extends FPDF
+    {
+        function Header()
+        {
+            $this->SetFont('Arial','U',15);
+            $this->Cell(170,10,utf8_decode('Fichas Técnicas'),0,0,'C');
+            $this->Ln(20);
+        }
+
+        function Footer()
+        {
+            $this->SetY(-15);
+            $this->SetFont('Arial','I',8);
+            $this->Cell(0,10,utf8_decode('Página').$this->PageNo().'/{nb}',0,0,'C');
+        }
+    }
+
+    require '../coneccion/datos_coneccion.php';
+    
+    $consulta = "SELECT * FROM perfil";
+    $resultado = $conexion->query($consulta);
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',11);
+    $pdf->SetFillColor(17,27,84);
+
+    while($row=$resultado->fetch_assoc())
+    {   
+        $pdf->SetTextColor(255,255,255);
+        $pdf->Cell(170,10,utf8_decode($row['NOMBRE_PERFIL']),1,1,'C',true);
+        $pdf->SetTextColor(0,0,0);
+        $pdf->Cell(170,10,utf8_decode($row['TECLADO_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['RAM_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['VIDEO_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['MOUSE_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['GABINETE_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['CPU_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['MONITOR_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['DISCODURO_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['UNIDADES_DESCRIPCION']),1,1,'',0);
+        $pdf->Cell(170,10,utf8_decode($row['VARIOS_DESCRIPCION']),1,1,'',0);
+        $pdf->Ln(20);
+    }
+    
+    $pdf->Output();
+?>
