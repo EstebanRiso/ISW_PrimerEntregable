9793f89ed97a76a771815df6df2b9948a2bf7d33
Nicolas_A <reyesaravena_nico@hotmail.com>
2021-05-05 20:00:42 -0400

    alerta_vencimiento_2

--- contratos/vencimiento.php
+++ contratos/vencimiento.php
@@ -1,11 +1,18 @@
 
-<script type="text/javascript">
-    alert(
-    <?php
-        echo "alerta";
+<?php require('../coneccion/datos_coneccion.php');
 
-            
+    
         
-    ?>
-    )
-</script>
+
+        $consulta= "SELECT CURRENT_DAY";
+    
+        $resultado= mysqli_query($conexion,$consulta);
+        
+
+        
+    
+        echo "$resultado";
+        echo '<script type="text/javascript">alert(\' Precaución, plazo del documento n° [] vencerá dentro de [] meses \');</script>'; 
+
+
+?>
